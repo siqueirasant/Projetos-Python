831458d2c1530c7655f70e70a09f85b47b87aedf
siqueirasant <siqueirasant@gmail.com>
2021-09-02 20:58:10 -0300

    Atualização Funcoes, arquivo anterior incomplento

--- Projeto 1 - Funcoes.py
+++ Projeto 1 - Funcoes.py
@@ -1,39 +1,54 @@
-#Função que lê os caracteres de uma string e extrai o número da nota.
-
-def extrai_nf(texto):
-    nota = ''
-    for x in texto:
-        if x.isdigit() == True:
-            nota = nota + x
-    return nota
-
-
-#Função que lê os caracteres de uma string e extrai o nome do fornecedor.
-def extrai_forn(texto):
-    inicio = 'PAGTO A EFETUAR DOC. NRO. DO FORN.  '
-    novo_texto = ""
-    for caractere in texto:
-        if caractere.isdigit() == False:
-            novo_texto = novo_texto + caractere
-    fornecedor = novo_texto[len(inicio):56]
-    return fornecedor
+from datetime import datetime
 
 '''
 Percorre todas as planilhas do arquivo excel, 
-Le cada nome, coloca numa lista
+Lê cada nome, coloca numa lista
 '''
-def lista_plan_nomes():
+def lista_plan_nomes(arquivo):
     lista = []
     for plan in arquivo.sheetnames:
         lista.append(plan)
     return lista
 
-#Função que calcula a identificação da célula
-def calcula_num(coluna, max_linha):
-    num = 1
-    lista_celula = []
-    while num < max_linha:
-        celula = str(coluna) + str(num)
-        lista_celula.append(celula)
-        num = num + 1
-    return lista_celula
+def extrai_desc_aprop(texto):
+    inicio = 'PAGTO A EFETUAR '
+    nome_forn = ""
+    for caractere in texto:
+        nome_forn = nome_forn + caractere
+    descricao = nome_forn[len(inicio):60]  
+    return descricao
+
+def extrai_desc_baixa(texto):
+    inicio = 'PAGTO DO '
+    nome_forn = ''
+    for caratere in texto:
+        nome_forn = nome_forn + caratere
+        descricao = nome_forn[len(inicio):53]
+    return descricao
+
+def data(texto):
+    if type(texto) == str:
+        dataf = texto
+    else:
+        data = datetime.date(texto)
+        dia = data.day
+        mes = data.month
+        ano = data.year
+        dataf = f'{dia}/{mes}/{ano}'
+    return dataf
+
+def extrai_bx_cli(texto):
+    inicio = "RECEBIMENTO "
+    nome_cliente = texto[len(inicio):43]
+    return nome_cliente
+
+def extrai_provisao_cli(texto):
+    provisao = texto[0:31]
+    return provisao
+
+def num_nd_cli(texto):
+    num = ''
+    for caractere in texto:
+        if caractere.isdigit() == True:
+            num = num + caractere
+    return num
